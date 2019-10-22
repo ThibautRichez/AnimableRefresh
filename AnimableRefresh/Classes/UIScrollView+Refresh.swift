04513db8f781e79336fc0ce8ceaf532ff74d1ac9
Thibaut Richez <thibaut.richez@benextcompany.com>
2019-10-22 09:35:29 +0200

    Add custom refresh

---
+++ AnimableRefresh/Classes/UIScrollView+Refresh.swift
@@ -0,0 +1,42 @@
+//
+//  UIScrollView+Refresh.swift
+//  Refresh
+//
+//  Created by Thibaut Richez on 10/19/19.
+//  Copyright © 2019 Thibaut Richez. All rights reserved.
+//
+
+import UIKit
+
+extension UIScrollView {
+    /// The custom `RefreshView` associated to the `scrollView`
+    /// - Note: You must call `addRefresh(custom:action:)` before using this value
+    public var refreshView: RefreshView? {
+        return subviews.first { $0 is RefreshView } as? RefreshView
+    }
+
+    /// Add a pull to refresh effect to a `UIScrollView` or any of its subclasses (`UITableView` -
+    /// `UICollectionView`).
+    ///- Parameters:
+    ///    - custom: The view that you want to be animated during the `action` process.
+    ///    If not provided, a system like view is shown (`SystemLoaderView`)
+    ///    - action: The action you want to be triggered on loading process
+    public func addRefresh(custom view: AnimableView = SystemLoaderView(), action: (() -> Void)?) {
+        let refresherView = RefreshView(animableView: view, action: action)
+        refresherView.scrollView = self
+        addSubview(refresherView)
+        sendSubviewToBack(refresherView)
+    }
+
+    /// Tells the view that the refresh operation was started programmatically.
+    /// - Note: You must call `addRefresh(custom:action:)` before calling this method
+    public func startRefresh() {
+        refreshView?.startRefresh()
+    }
+
+    /// Tells the view that the refresh operation has ended.
+    /// - Note: You must call `addRefresh(custom:action:)` before calling this method
+    public func endRefresh() {
+        refreshView?.endRefresh()
+    }
+}
---
+++ Example/Pods/Target Support Files/AnimableRefresh/AnimableRefresh-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_AnimableRefresh : NSObject
+@end
+@implementation PodsDummy_AnimableRefresh
+@end
